fad1c1dbe11517d4a0beb09b2056191d2f291d56
Chris Cooper <cjcooper252@gmail.com>
2017-03-05 10:50:51 +0000

    sets up routes folders

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const app = express();
 const config = require('./config/config');
 
+const index = require('./routes/index');
 const clients = require('./routes/clients');
+
+const app = express();
+
+app.use('/', index);
+app.use('/clients', clients);
 
 if(process.env.NODE_ENV === "test"){
   app.listen(config.test_port, function() {
@@ -16,4 +21,22 @@ if(process.env.NODE_ENV === "test"){
 
 app.get('/', (req, res) => {
   res.send('hello world');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
