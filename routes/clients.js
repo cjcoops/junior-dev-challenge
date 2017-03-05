7b3d5037dd0bf2bfdd7140e306fc449006e6c3f6
Chris Cooper <cjcooper252@gmail.com>
2017-03-05 17:20:22 +0000

    creates client show view

--- routes/clients.js
+++ routes/clients.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var Client = require('../models/client');
+var Candidate = require('../models/candidate');
 
 router.get('/', function(req, res, next) {
   Client.find(function(err, clients) {
-    if (err) console.log(err)
-    res.render('clients/index', {clients: clients})
+    if (err) console.log(err);
+    res.render('clients/index', {clients: clients});
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  Client.findById(req.params.id, function(err, client) {
+    if (err) console.log(err);
+    Candidate.find(function(err, candidates) {
+      if (err) console.log(err);
+      res.render('clients/show', {client: client, candidates: candidates});
+    })
   });
 });
 
